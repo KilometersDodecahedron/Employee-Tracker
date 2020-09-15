e04484a6f3c7ac1b43ef85f2a240ef33260569e8
Miles Cohn <milesselim@icloud.com>
2020-09-15 12:57:17 -0400

    Splitting the inquirer prompts between scripts based on the type of function they serve. Using module.exports to have them communicate

---
+++ addDataEntries.js
@@ -0,0 +1,13 @@
+// const openingMenu = require("./index.js");
+// const
+
+const inquirer = require("inquirer");
+const mysql = require("mysql");
+
+const decidedToViewData = () => {
+    console.log("this is addDataEntries.js");
+}
+
+const foo = 6;
+
+module.exports = { viewData: decidedToViewData, foo: foo};
--- index.js
+++ index.js
@@ -1,5 +1,10 @@
+const viewData = require("./addDataEntries");
+
 const inquirer = require("inquirer");
 const mysql = require("mysql");
+
+//put in a new line by adding this as a template literal
+const newLine = `\n-------------------------------`;
 
 var connection = mysql.createConnection({
     host: "localhost",
@@ -15,4 +20,57 @@ var connection = mysql.createConnection({
     database: "greatBay_DB"
 });
 
+//easy way to separate lines, without having to worry about reformatting the text for template literals
+const makeNewLine = () => {
+    console.log(newLine);
+}
+
+//run when a connection needs to end
+const endConnection = () => {
+    makeNewLine();
+    console.log("Thank you for using Golden Child Employee Tracker! \nIf you'd like to run the program again, simply run\nnode index.js\nIn the program's directory\n-------------------------------");
+    connection.end();
+}
+
 //for the FOREIGN KEYS (FK) load the data, and generate the options from the saved data
+
+//run at the start, and after each action is complete to return to the opening menu
+const openingMenu = () => {
+    makeNewLine();
+    console.log(`Welcome to the Golden Child Employee Tracker!`);
+    makeNewLine();
+    inquirer.prompt([
+        {
+            type: "list",
+            message: "What would you like to do?",
+            name: "action",
+            choices: ["View Existing Entries", "Enter New Information", "Update Employee Roles", "Exit Golden Child Employee Tracker"]
+        }
+    ]).then(answer => {
+        console.log(answer.action);
+        switch(answer.action){
+            case "View Existing Entries":
+                break;
+            case "Enter New Information":
+                break;
+            case "Update Employee Roles":
+                break;
+            case "Exit Golden Child Employee Tracker":
+                endConnection();
+                break;
+            default:
+                console.log("ERROR: openingMenu() is breaking switch statement");
+                connection.end();
+        }
+    });
+}
+
+module.exports = openingMenu;
+module.exports = newLine;
+
+connection.connect(function(err) {
+    if (err) throw err;
+    // console.log("connected as id " + connection.threadId + "\n");
+    viewData.viewData();
+    openingMenu();
+});
